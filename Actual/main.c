163af239821ba13759cc81bd371aa6a0616f5804
Immanuel I George <ikp4success@yahoo.com>
2015-04-19 21:01:47 -0400

    main.c

--- Actual/main.c
+++ Actual/main.c
@@ -1,13 +1,14 @@
 /**
 *   Name:       Immanuel I George, Nicholas White, Jason Hindulak
-*   Group:      C
-*   Date:       12-APR-2015
-*   Project 3:  Floppy Shell Program
-*   Goal:       Add additional UNIX commands to the Floppy shell program
+*   Group:      P
+*   Date:       19-APR-2015
+*   Project 3:  Floppy Shell Implementation
+*   Goal:       Add additional UNIX commands to the Floppy Shell project, add inter-process communication functionality.
 *
-*   UPDATED:    12-APR-2015
+*   UPDATED:    19-APR-2015
 **/
 
+//System Libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,26 +16,38 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+//Project Libraries
 #include "commands.h"
 #include "processhandling.h"
 
-#define BUFFER_SIZE 1024
+//Macros
+#define BUFFER_SIZE 1024	//Buffer
 
-#define COMMAND_EXECUTE 0
+#define clearScreen() write(1,"\033[H\033[J",sizeof("\033[H\033[J"))
+//#define clearScreen() printf("\033[H\033[J")
+#define COMMAND_EXECUTE 0	//Command sub-set
 #define COMMAND_QUIT 1
 #define COMMAND_CLEAR 2
 #define COMMAND_PATH_MOD 3
 #define COMMAND_CWD 4
-
 #define FLOP_MOUNT 5
 #define FLOP_UMOUNT 6
-
-
 
 /*
 * Appends a directory to the end of the current PATH.
 */
 void addPath(char *add) {
+	//Using chdir
+	/*
+	char *directory = add;
+	int ret;
+	ret = chdir (directory);
+	if(0 != ret){
+		write(1,"chdir system call failed!",43-18);
+	}
+	*/
+	
 	char *path = getenv("PATH");
 	char *newpath = malloc(strlen(path) + strlen(add) + 2);
 
@@ -44,12 +57,23 @@ void addPath(char *add) {
 	setenv("PATH", newpath, 1);
 
 	free(newpath);
+
 }
 
 /*
 * Deletes all instances of a directory from the PATH.
 */
 void delPath(char *rem) {
+
+	/*
+	char *prev = rem;
+	prev = getcwd(prev, 0);
+
+	chdir(prev);
+	free(prev);
+	*/ 
+
+
 	char *path = (char *)malloc((sizeof(char) * strlen(getenv("PATH"))) - (sizeof(char) * strlen(rem)));
 	path[0] = '\0';
 	int i = 0; char *p;
@@ -64,6 +88,7 @@ void delPath(char *rem) {
 		}
 	}
 	setenv("PATH", path, 1);
+
 }
 
 /*
@@ -72,15 +97,42 @@ void delPath(char *rem) {
 void printPrompt() {
 	char cwd[BUFFER_SIZE];
 	getcwd(cwd, sizeof(cwd));
+
 	printf("[\033[1;33m%s\033[0m]shell\033[1;36m$\033[0m ", cwd);
+	//Or
+	//write(1,"$",sizeof("$"));
+}
+
+void mountFloppyFirst(char *pathFloppyLoc){
+    if (access(pathFloppyLoc, F_OK) != -1) {
+        mount(pathFloppyLoc);
+    } else {
+        
+        //Can't figure out how to pass variable into write system call...
+        //write(1,"    `",sizeof("    `"));
+        //write(1,&pathFloppyLoc,sizeof(pathFloppyLoc));
+        //write(1,"` does not exist. Please use a valid image.\n",sizeof("` does not exist. Please use a valid image.\n"));
+        printf("    `%s` does not exist. Please use a valid image.\n", pathFloppyLoc);
+    }
 }
 
 /*
-* Clears the shell screen.
+* WARNING: I removed this function and replaced it with a simple #define clear() at the top
+* - Nick White
+	
+	void clearScreen() {
+		write(1,"\033[H\033[J",sizeof("\033[H\033[J"));
+		// printf("\033[H\033[J");
+	}	
+
 */
-void clearScreen() {
-	printf("\033[H\033[J");
-}
+
+	// void clearScreen() {
+	// 	//write(1,"\033[H\033[J",sizeof("\033[H\033[J"));
+	// 	printf("\033[H\033[J");
+	// }	
+
+
 
 /*
 * Processes internal commands that can be handled later.
@@ -102,19 +154,13 @@ int process_internal_command(char* cmd){
     // if (strstr(cmd, "fmount") == cmd) {
     //     return FLOP_MOUNT;
     // }
-    if (strstr(cmd, "fumount") == cmd) {
-        return FLOP_UMOUNT;
-    }
+    // if (strstr(cmd, "fumount") == cmd) {
+    //     return FLOP_UMOUNT;
+    // }
 	return COMMAND_EXECUTE;
 }
 
-void mountFloppy(char *pathFloppyLoc){
-	if (access(pathFloppyLoc, F_OK) != -1) {
-		mount(pathFloppyLoc);
-	} else {
-		printf("    `%s` does not exist. Please use a valid image.\n", pathFloppyLoc);
-	}
-}
+
 
 
 
@@ -122,14 +168,18 @@ void mountFloppy(char *pathFloppyLoc){
 * Main
 */
 int main(int argc, char** argv){
-	clearScreen();
+	clearScreen(); //Clear any existing output in terminal
     addPath(".");
 	size_t buffer = BUFFER_SIZE;
 	char* cmd = (char*)malloc(BUFFER_SIZE);
-	int c = 1;
+
+	//WARNING: Hard-coded, ensure this is in the README. 
 	char* pathFloppyLoc = "imagefile.img";
-	mountFloppy(pathFloppyLoc);
-	while (c) {
+	mountFloppyFirst(pathFloppyLoc);
+
+	int continueRunning = 1;
+	while (continueRunning) {
+
 		printPrompt();
 		getline(&cmd, &buffer, stdin);
 		char* ptr = cmd;
@@ -146,7 +196,7 @@ int main(int argc, char** argv){
              * EXIT
              */
 			case COMMAND_QUIT: {
-				c = 0;
+				continueRunning = 0;
 				break;
 			}
 
@@ -164,7 +214,7 @@ int main(int argc, char** argv){
                     ptr++;
 					delPath(ptr+1);
 				}
-
+				//write(1,getenv("PATH"),sizeof(getenv("PATH")));
                 printf("%s\n", getenv("PATH"));
 				break;
 			}
@@ -182,6 +232,7 @@ int main(int argc, char** argv){
 				} else {
                     //Change to user defined directory
 					if(chdir(ptr)<0) {
+						//write(1,)
 						printf("Error: No such directory exists [\"%s\"].\n",ptr);
 					}
 				}
@@ -219,26 +270,26 @@ int main(int argc, char** argv){
             /*
              * UNMOUNT
              */
-            case FLOP_UMOUNT: {
+            // case FLOP_UMOUNT: {
 
-                ptr+=strlen("FUMOUNT");
-                while(*ptr==' ')
-                    ptr++;
+            //     ptr+=strlen("FUMOUNT");
+            //     while(*ptr==' ')
+            //         ptr++;
 
-                //Check if mounted
-                if (!isMounted()) {
-                    printf("    ERROR: no floppy mounted.\n");
-                    break;
-                }
+            //     //Check if mounted
+            //     if (!isMounted()) {
+            //         printf("    ERROR: no floppy mounted.\n");
+            //         break;
+            //     }
 
-                //check for correct usage
-                if (*ptr == '\0') {
-                    unmount();
-                } else {
-                    printf("Usage: fumount\n");
-                }
-                break;
-            }
+            //     //check for correct usage
+            //     if (*ptr == '\0') {
+            //         unmount();
+            //     } else {
+            //         printf("Usage: fumount\n");
+            //     }
+            //     break;
+            // }
 
             /*
              * CLEAR
