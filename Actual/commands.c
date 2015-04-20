32565510fb5b3197cea78e47768d5ebfab3724f5
Immanuel I George <ikp4success@yahoo.com>
2015-04-19 21:00:49 -0400

    commands.c

--- Actual/commands.c
+++ Actual/commands.c
@@ -1,18 +1,30 @@
+/**
+*   Name:       Immanuel I George, Nicholas White, Jason Hindulak
+*   Group:      P
+*   Date:       19-APR-2015
+*   Project 3:  Floppy Shell Implementation
+*   Goal:       Add additional UNIX commands to the Floppy Shell project, add inter-process communication functionality.
+*
+*   UPDATED:    19-APR-2015
+**/
+
+//System Libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "commands.h"
-#include "Fat12BootEntry.h"
 #include <unistd.h>  
 #include <fcntl.h>
-
 #include <errno.h>
 
-//Define global variables
-int img ;
+//Project Libraries
+#include "commands.h"
+#include "Fat12BootEntry.h"
 
-int mounted = 0;
-char *filename = '\0';
+
+//Define global variables
+int img;    //Image file provided
+int mounted = 0;    //Set to 1 when mounted.
+char *filename = "";
 
 //Define FAT12Boot Structure
 Fat12Boot boot;
@@ -21,10 +33,9 @@ Fat12Entry *entry;
 /*
 *   Loads all FAT12 boot information into 'boot' and FAT12 entries into 'entry'
 */
-void load() {
+void load() { //Should this be seek not lseek???
     lseek(img, 11, SEEK_SET); //skip 11 bytes
     //lseek();
-
     //BYTES_PER_SECTOR (2 bytes)
     //fread(&boot.BYTES_PER_SECTOR, 2, 1, img);
     read(img,&boot.BYTES_PER_SECTOR,2);
@@ -123,9 +134,9 @@ void load() {
    // printf("IMG: %d\n", img);
 }
 
-/*
-*   Clears 'boot'
-*/
+
+
+//This function clears the boot struct, and entry struct
 void unload() {
     boot = (Fat12Boot){0};
     entry = (Fat12Entry *){0};
@@ -135,63 +146,104 @@ void unload() {
 *   Saves file information and opens the file
 */
 void mount(char *file) {
-    
-    filename = (char *)malloc(strlen(file) + 1);
+   
+    filename = (char *)malloc(strlen(file) + 1); //WHERE fn is FileName from Project 2
     strcpy(filename, file);
     img = open(file, O_RDONLY);
     mounted = 1;
     load();
+    //write(1,"Done!\n",6);
     //printf("Done!\n");
 }
 
-/*
-*   Clears  file information and closes the file
-*/
+/*Clears file information and closes the file */
 void unmount() {
-    printf("    Unmounting `%s`... ", filename);
+    printf("    Unmounting the floppy image for you `%s`... ", filename);
     filename = '\0';
     close(img);
     mounted = 0;
     unload();
-    printf("Done!\n");
+    write(1,"Done!\n",6);
+    //printf("Done!\n");
+}
+
+void mountFloppySecond(char *pathFloppyLoc){
+    if (access(pathFloppyLoc, F_OK) != -1) {
+        mount(pathFloppyLoc);
+    } else {
+        
+        //Can't figure out how to pass variable into write system call...
+        //write(1,"    `",sizeof("    `"));
+        //write(1,&pathFloppyLoc,sizeof(pathFloppyLoc));
+        //write(1,"` does not exist. Please use a valid image.\n",sizeof("` does not exist. Please use a valid image.\n"));
+        printf("    `%s` does not exist. Please use a valid image.\n", pathFloppyLoc);
+    }
 }
 
 /*
 *   Prints FAT12 image structure
 */
 void structure(int l) {
-    printf("    Mounted\n");
+    //WARNING: Hard-coded, ensure this is in the README. 
+    char* pathFloppyLoc = "imagefile.img";
+    mountFloppySecond(pathFloppyLoc);
+     write(1,"Mounting the floppy for you now...\n",35);
+
+    //printf("    Mounted\n");
     printf("        number of FAT:                      %d\n", boot.NUM_OF_FATS);
     printf("        number of sectors used by FAT:      %d\n", boot.SECTORS_PER_FAT);
     printf("        number of sectors per cluster:      %d\n", boot.SECTORS_PER_CLUSTER);
     printf("        number of ROOT Entries:             %d\n", boot.MAX_ROOT_DIRS);
     printf("        number of bytes per sector          %d\n", boot.BYTES_PER_SECTOR);
     if (l) {
-        printf("        ---Sector #---      ---Sector Types---\n");
-        printf("             0                    BOOT\n");
+        write(1,"        ---Sector #---      ---Sector Types---\n",47);
+        write(1,"             0                    BOOT\n",39);
+        //printf("        ---Sector #---      ---Sector Types---\n");
+        //printf("             0                    BOOT\n");
 
         for(int i = 0; i < boot.NUM_OF_FATS; i++)
                 printf("          %02d -- %02d                FAT%d\n", (boot.SECTORS_PER_FAT * i) + 1, boot.SECTORS_PER_FAT * (i + 1), i);
 
         printf("          %02d -- %02d                ROOT DIRECTORY\n", boot.SECTORS_PER_FAT * boot.NUM_OF_FATS, (boot.MAX_ROOT_DIRS / 16) + (boot.SECTORS_PER_FAT * boot.NUM_OF_FATS));
     }
+    printf("\n");
+    unmount();
+    printf("\n");
 }
 
 /*
 *   Lists all files within the FAT12 image
 */
 void traverse(int l) {
+      char* pathFloppyLoc = "imagefile.img";
+        mountFloppySecond(pathFloppyLoc);
+         write(1,"Mounting the floppy for you now...\n",35);
     if (l) {
-        printf("    Mounted\n");
-        printf("    *****************************\n");
-        printf("    ** FILE ATTRIBUTE NOTATION **\n");
-        printf("    **                         **\n");
-        printf("    ** R ------ READ ONLY FILE **\n");
-        printf("    ** S ------ SYSTEM FILE    **\n");
-        printf("    ** H ------ HIDDEN FILE    **\n");
-        printf("    ** A ------ ARCHIVE FILE   **\n");
-        printf("    *****************************\n");
-        printf("\n");
+        //WARNING: Hard-coded, ensure this is in the README. 
+      
+
+        write(1,"    Mounted\n",13);
+        write(1,"    *****************************\n",34);
+        write(1,"    ** FILE ATTRIBUTE NOTATION **\n",34);
+        write(1,"    **                         **\n",34);
+        write(1,"    ** R ------ READ ONLY FILE **\n",34);
+        write(1,"    ** S ------ SYSTEM FILE    **\n",34);
+        write(1,"    ** H ------ HIDDEN FILE    **\n",34);
+        write(1,"    ** A ------ ARCHIVE FILE   **\n",34);
+        write(1,"    *****************************\n",34);
+        write(1,"\n",1);
+
+        //printf("    Mounted\n");
+        //printf("    *****************************\n");
+        //printf("    ** FILE ATTRIBUTE NOTATION **\n");
+        //printf("    **                         **\n");
+        //printf("    ** R ------ READ ONLY FILE **\n");
+        //printf("    ** S ------ SYSTEM FILE    **\n");
+        //printf("    ** H ------ HIDDEN FILE    **\n");
+        //printf("    ** A ------ ARCHIVE FILE   **\n");
+        //printf("    *****************************\n");
+        //printf("\n");
+
         //printf("jsrjkfsksbsf%d\n", boot.MAX_ROOT_DIRS);
         for (int i = 0; i < boot.MAX_ROOT_DIRS; i++) {
             if (entry[i].FILENAME[0] != 0x00 && entry[i].START_CLUSTER != 0) {
@@ -235,34 +287,49 @@ void traverse(int l) {
             }
         }
     }
+    printf("\n");
+    unmount();
+    printf("\n");
 }
 
 /*
 *   Outputs a hex dump of all FAT tables
 */
 void showfat() {
-    printf("    Mounted\n");
+    //WARNING: Hard-coded, ensure this is in the README. 
+    char* pathFloppyLoc = "imagefile.img";
+    mountFloppySecond(pathFloppyLoc);
+     write(1,"Mounting the floppy for you now...\n",35);
+
+    write(1,"    Mounted\n",12);
+    //printf("    Mounted\n");
+
     unsigned char in;
     char * free = "FREE";
 
     lseek(img, boot.BYTES_PER_SECTOR, SEEK_SET);
 
     printf("        0    1    2    3    4    5    6    7    8    9    a    b    c    d    e    f\n");
+    write(1,"        0    1    2    3    4    5    6    7    8    9    a    b    c    d    e    f\n",99-14);
     for (int i = 0; i < (boot.NUM_OF_FATS * boot.SECTORS_PER_FAT * boot.BYTES_PER_SECTOR); i++) {
         if (i % 16 == 0 || i == 0) {
+            //write(1,"\n",2);
             printf("\n");
             printf("%4x", i);
         }
         //fread(&in, 1, 1, img);
         read(img,&in,1);
        if(in == 0){
-printf("%5s",free);
+        printf("%5s",free);
 }
 else{
     printf("%5x", in);
 }
     }
 
+    write(1,"\n",1);
+     printf("\n");
+     unmount();
     printf("\n");
 }
 
@@ -270,7 +337,9 @@ else{
 *   Outputs a hex dump of a specific sector (512 bytes)
 */
 void showsector(int sector) {
-    printf("    Mounted\n");
+   char* pathFloppyLoc = "imagefile.img";
+    mountFloppySecond(pathFloppyLoc);
+     write(1,"Mounting the floppy for you now...\n",35);
     unsigned char in;
 
     lseek(img, boot.BYTES_PER_SECTOR * sector, SEEK_SET);
@@ -286,58 +355,11 @@ void showsector(int sector) {
         printf("%5x", in);
     }
 
+    write(1,"\n",1);
+    printf("\n");
+    unmount();
     printf("\n");
 }
-
-/*
-*   Outputs a hex dump of a specific file
-*/
-// void showfile(char *file) {
-//     printf("    Mounting `%s`... ", file);
-//     char *filename = '\0';
-//     char *ext = '\0';
-//     char *full_filename;
-//     char *p;
-
-//     //Parse filename
-//     p = strtok(file, ".");
-//     if (p)
-//         filename = p;
-
-//     //Parse extension
-//     p = strtok(NULL, ".");
-//     if (p)
-//         ext = p;
-
-//     full_filename = (char *)malloc(strlen(filename) + strlen(ext) + 1);
-//     strcpy(full_filename, filename);
-//     strcat(full_filename, ext);
-
-//     for (int i = 0; i < boot.MAX_ROOT_DIRS; i++) {
-//         if (entry[i].FILENAME != 0x00 && entry[i].START_CLUSTER != 0) { //Ignore invalid files
-//             if(equals(full_filename, (char *)entry[i].FILENAME)) {
-//                 unsigned char in;
-
-//                 //Move to the first byte of the file
-//                 lseek(img, ((boot.MAX_ROOT_DIRS / 16) + (boot.SECTORS_PER_FAT * boot.NUM_OF_FATS) - 1) + (boot.BYTES_PER_SECTOR * entry[i].START_CLUSTER), SEEK_SET);
-
-//                 printf("        0    1    2    3    4    5    6    7    8    9    a    b    c    d    e    f\n");
-//                 for (int j = 0; j < entry[i].FILE_SIZE; j++) {
-//                     if (j % 16 == 0 || j == 0) {
-//                         printf("\n");
-//                         printf("%4x", j);
-//                     }
-//                     //fread(&in, 1, 1, img);
-//                     read(img,&in,1);
-//                     printf("%5x", in);
-//                 }
-
-//                 printf("\n");
-//             }
-//         }
-//     }
-// }
-
 /*
 *   Compares two char*'s exactly
 *   Used for comparing FILENAME's
@@ -363,3 +385,65 @@ char *getFile() {
 int isMounted() {
     return mounted;
 }
+
+void help() {
+    write(1,"Here are some commands supported by the floppy console:\n",70-14);
+    write(1,"\tfumount: Unmounts the floppy images.\n",53-14);
+    write(1,"\t   Example: 'fumount'\n",38-14);
+    write(1,"\tstructure: Lists the structure of the floppy disk.\n",67-14);
+    write(1,"\t   Example: 'structure [-l]'\n",-14);
+    write(1,"\ttraverse: Shows contents with root directory.\n",62-14);
+    write(1,"\t   Example: 'traverse [-l]'\n",44-14);
+    write(1,"\tshowfat: Show FAT tables.\n",42-14);
+    write(1,"\t   Example: 'showfat'\n",38-14);
+    write(1,"\tshowsector: Shows the contents of a sector.\n",60-14);
+    write(1,"\t            Add '> [file]' to create file and save sector info.\n",80-14);
+    write(1,"\t   Example: 'showsector [number]'\n",50-14);
+    write(1,"\t            'showsector [number] > [file]'\n",59-14);
+    write(1,"\thelp: Shows commands.\n",38-14);
+    write(1,"\t   Example: 'help'\n",35-14);
+    write(1,"\texit: Exits the floppy program.\n",48-14);
+    write(1,"\t   Example: 'exit'\n",35-14);
+    write(1,"Here are some common commands that are supported:\n",64-14);
+    write(1,"\tcd: Changes directory.\n",39-14);
+    write(1,"\t   Example: 'cd', 'cd ..', 'cd /home'\n",54-14);
+    write(1,"\tpath: Shows the current pathnames with additional utility to\n",77-14);
+    write(1,"\t      add/remore command searching pathnames\n",61-14);
+    write(1,"\t   Example: 'path'\n",35-14);
+    write(1,"\t            'path +'\n",37-14);
+    write(1,"\t            'path -'\n",37-14);
+    write(1,"\tcmd1 | cmd2: Pipeline command to connect output of cmd1 to input of cmd2\n",89-14);
+    write(1,"\t   Example: 'cmd1 | cmd2'\n",42-14);
+    write(1,"*Floppy program also supports all Linux Shell Commands*\n",70-14);
+    //Wow, that took a while - Nick
+    /*
+	printf("Here are some commands supported by the floppy console:\n");
+	printf("\tfumount: Unmounts the floppy images.\n");
+	printf("\t   Example: 'fumount'\n");
+	printf("\tstructure: Lists the structure of the floppy disk.\n");
+	printf("\t   Example: 'structure [-l]'\n");
+	printf("\ttraverse: Shows contents with root directory.\n");
+	printf("\t   Example: 'traverse [-l]'\n");
+	printf("\tshowfat: Show FAT tables.\n");
+	printf("\t   Example: 'showfat'\n");
+	printf("\tshowsector: Shows the contents of a sector.\n");
+	printf("\t            Add '> [file]' to create file and save sector info.\n");
+	printf("\t   Example: 'showsector [number]'\n");
+	printf("\t            'showsector [number] > [file]'\n");
+	printf("\thelp: Shows commands.\n");
+	printf("\t   Example: 'help'\n");
+	printf("\texit: Exits the floppy program.\n");
+	printf("\t   Example: 'exit'\n");
+	printf("Here are some common commands that are supported:\n");
+	printf("\tcd: Changes directory.\n");
+	printf("\t   Example: 'cd', 'cd ..', 'cd /home'\n");
+	printf("\tpath: Shows the current pathnames with additional utility to\n");
+	printf("\t      add/remore command searching pathnames\n");
+	printf("\t   Example: 'path'\n");
+	printf("\t            'path +'\n");
+	printf("\t            'path -'\n");
+	printf("\tcmd1 | cmd2: Pipeline command to connect output of cmd1 to input of cmd2\n");
+	printf("\t   Example: 'cmd1 | cmd2'\n");
+	printf("*Floppy program also supports all Linux Shell Commands*\n");
+    */
+} 
